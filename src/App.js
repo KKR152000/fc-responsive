1b8c84989d13f517b14e6657c766c75a417a6360
KKR152000 <79276300+KKR152000@users.noreply.github.com>
2021-03-25 16:14:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Sec1 from './components/Sec1';
+import Sec2 from './components/Sec2';
+import Sec3 from './components/Sec3';
+import Sec4 from './components/Sec4';
+import Sec5 from './components/Sec5';
+import Sec6 from './components/Sec6';
+import Sec7 from './components/Sec7';
+import Sec8 from './components/Sec8';
+import Sec9 from './components/Sec9';
+import Sec10 from './components/Sec10';
+
+function App() {
+  return (
+    <div className="App">
+     <Sec1 />
+     <Sec2 />
+     <Sec3 />
+     <Sec4 />
+     <Sec5 />
+     <Sec6 />
+     <Sec7 />
+     <Sec8 />
+     <Sec9 />
+     <Sec10 />
+    </div>
+  );
+}
+
+export default App;
